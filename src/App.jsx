a51772c22d6f5a3f50b7748ac406ba8d1293817c
Zeuhz-Droid <davidadegbiran2020@gmail.com>
2023-12-18 20:08:07 +0100

    feat: compilation of all comp

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import HowToBuy from "./components/HowToBuy";
+import Tokenomics from "./components/Tokenomics";
+
+function App() {
+  return (
+    <div>
+      <Hero />
+      <Tokenomics />
+      <HowToBuy />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
