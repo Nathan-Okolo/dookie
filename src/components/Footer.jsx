d4fb5695788661e3d88b8c506ae1063cf81e9d45
Zeuhz-Droid <davidadegbiran2020@gmail.com>
2023-12-18 20:07:51 +0100

    feat: footer comp

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const StyledFooter = styled.div`
+  text-align: center;
+  background-color: #ba7043;
+  color: #ffffff;
+  padding: 1rem;
+  width: 100%;
+`;
+
+function Footer() {
+  return <StyledFooter>Just Dookit 2023</StyledFooter>;
+}
+
+export default Footer;
